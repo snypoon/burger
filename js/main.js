53995f99a4dc5a0d073fe670cbeae87bc8c2b22d
snypoon <snypoon2202@gmail.com>
2018-11-26 00:56:26 +0300

    new

--- js/main.js
+++ js/main.js
@@ -3,24 +3,24 @@
 const menu = document.querySelector('#hamburger__menu');
 const link = document.querySelector('#hamburger-menu__link');
 const bars = document.querySelector('#hamburger-menu__bars');
-const body = document.querySelector('body');
+var body = document.querySelector('body');
 const nav = document.querySelector('.hamburger__nav');
 
-let activeMenu = function() {
+let activeMenu = function () {
   menu.classList.toggle('active');
   bars.classList.toggle('bars--active');
   body.classList.toggle('menu--active');
 };
 
-link.addEventListener('click', function(e){
+link.addEventListener('click', function (e) {
   e.preventDefault();
   activeMenu();
 });
 
-nav.addEventListener('click', function(e){
+nav.addEventListener('click', function (e) {
   e.preventDefault();
   target = e.target;
-  if ( target.className = 'nav__link'){
+  if (target.className = 'nav__link') {
     activeMenu();
   };
 });
@@ -29,15 +29,14 @@ nav.addEventListener('click', function(e){
 
 let teamAction = () => {
   let teamLink = document.querySelectorAll('.team-acco__title');
-  teamLink.forEach(function(name){
-    name.addEventListener('click',function(e){
+  teamLink.forEach(function (name) {
+    name.addEventListener('click', function (e) {
       e.preventDefault();
       let teamActiveItem = document.querySelector('.team-acco__item.team-acco__item--active');
-      if(teamActiveItem){
+      if (teamActiveItem) {
         teamActiveItem.classList.remove('team-acco__item--active');
-
       }
-      if (!teamActiveItem || teamActiveItem.querySelector(".team-acco__title") !== this){
+      if (!teamActiveItem || teamActiveItem.querySelector(".team-acco__title") !== this) {
         let teamItem = this.closest('.team-acco__item');
         teamItem.classList.add('team-acco__item--active');
       }
@@ -50,15 +49,15 @@ teamAction();
 
 let menuAction = () => {
   let menuLink = document.querySelectorAll('.menu-acco__block');
-  menuLink.forEach(function(label){
-    label.addEventListener('click',function(e){
+  menuLink.forEach(function (label) {
+    label.addEventListener('click', function (e) {
       e.preventDefault();
       let menuActiveItem = document.querySelector('.menu-acco__item.menu-acco__item--active');
-      if(menuActiveItem){
+      if (menuActiveItem) {
         menuActiveItem.classList.remove('menu-acco__item--active');
 
       }
-      if (!menuActiveItem || menuActiveItem.querySelector(".menu-acco__block") !== this){
+      if (!menuActiveItem || menuActiveItem.querySelector(".menu-acco__block") !== this) {
         let menuItem = this.closest('.menu-acco__item');
         menuItem.classList.add('menu-acco__item--active');
       }
@@ -66,3 +65,65 @@ let menuAction = () => {
   })
 }
 menuAction();
+
+// Slider
+
+const left = document.querySelector("#left");
+const right = document.querySelector('#right');
+const items = document.querySelector('#items');
+const counter = items.children.length;
+const step = parseInt(getComputedStyle(items.firstElementChild).width);
+const minRight = 0;
+const maxRight = parseInt(getComputedStyle(items).width);
+let currentRight = 0;
+
+items.style.right = currentRight;
+
+right.addEventListener("click", function () {
+  if (currentRight < maxRight * (counter - 1)) {
+    currentRight += step;
+  } else {
+    currentRight = 0;
+    items.style.right = 0;
+  }
+  items.style.right = currentRight + "px";
+});
+
+left.addEventListener("click", function () {
+  if (currentRight > minRight) {
+    currentRight -= step;
+  } else {
+    currentRight = maxRight * (counter - 1);
+  }
+  items.style.right = currentRight + "px";
+});
+
+// Modal
+
+const modalOpenBtn = document.querySelector('#modal__open');
+const modalCloseBtn = document.querySelector("#modal__close");
+const modal = document.querySelector('#modal');
+const listReviuew = document.querySelector('.reviews__list');
+var body = document.querySelector('body');
+// var text = document.querySelector('.reviews__text');
+// var textcont = text.textContent;
+// var modaltext = document.querySelector('.modal__text');
+
+let activeModal = () => {
+  modal.classList.toggle('modal__active');
+  body.classList.toggle('menu--active');
+  // modaltext.textContent = textcont;
+};
+
+listReviuew.addEventListener('click',(e) => {
+  e.preventDefault();
+  let target = e.target;
+  if (target.classList.contains('modal__open')) {
+    activeModal();
+  }
+});
+
+modalCloseBtn.addEventListener('click',(e) => {
+  e.preventDefault();
+  activeModal();
+});
